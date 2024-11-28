b7c4758fcb780448164ab98e650c43dc264302e8
Maxim Ponomarev <ponomarevmv08@gmail.com>
2024-11-28 23:43:24 +0300

    first commit

---
+++ src/main/java/ponomarev/dev/pethandbook/PetHandbookApplication.java
@@ -0,0 +1,13 @@
+package ponomarev.dev.pethandbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetHandbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetHandbookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ponomarev/dev/pethandbook/PetHandbookApplicationTests.java
@@ -0,0 +1,13 @@
+package ponomarev.dev.pethandbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetHandbookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
